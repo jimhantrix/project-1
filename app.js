fa896e7d2a1c6ae974772207d75d2ddd731db34b
jimhantrix <jimtekanyo@gmail.com>
2016-05-19 16:02:26 -0700

    cards appearing on the div''s

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+
 $(document).ready(function() {
   game();
+
 });
 
-//fae cards
 function game() {
   function buildDeck() {
     var faceCards = {
       11: 'J',
       12: 'Q',
       13: 'K',
-      1: 'A'
+      1:  'A'
     },
-    //building a deck
     deck = {
       push: function(card) {
         this.cards.push(card);
@@ -19,11 +19,9 @@ function game() {
       cards: []
     },
     card;
-      // if card is k (faceCards)
+
     for(var c = 1; c <= 13; c++) {
-      //checking if card is A (face card)
       for(var n = 0; n <= 3; n++) {
-      //if card is !number card then its (face card)
         if('undefined' !== typeof faceCards[c]) {
           card = faceCards[c];
         }
@@ -33,22 +31,29 @@ function game() {
         deck.push(card);
       }
     }
+
     return deck;
   }
-
- //message prompt players
   function getPlayers() {
     var players = [];
-    while(true) {
+
+
+    while(players.length <= 2) {
       var name = prompt('Please enter the next player\'s name.' +
           '\nIf there are no more players just press enter.');
-      if(String(name).length === 0) break;
+
+      if(String(name).length === 0)
+          break;
+
+
       players.push(Player(name));
     }
-    console.log(players);
+
+    // console.log(players);
+
     return players;
+
   }
-  //keeping score of each players
   function Player(name) {
     return {
       name: name,
@@ -56,27 +61,36 @@ function game() {
       score: 0
     }
   }
-  //
   function deal(players, deck) {
     players.forEach(function(player) {
       for(var c = 0; c < 2; c++) {
         player.hand.push(deck.cards[Math.floor(Math.random() * deck.cards.length)]);
       }
+
     });
-    console.log('after deal');
-    console.log(players);
+    // console.log(players);
   }
 
   var deck = buildDeck(),
     players = getPlayers();
+
   if(players.length > 0) {
     deal(players, deck);
-    console.log(players, deck);
-    // while(true) {
-    //
-    // }
-  }
-  else {
+
+    console.log(players);
+
+    players.forEach(function(player){
+        console.log(players.hand);
+        var player1Card1 = document.getElementById("playerC1");
+        player1Card1.innerHTML = players[0].hand[0];
+        var player1Card2 = document.getElementById("playerC2");
+        player1Card2.innerHTML = players[0].hand[1];
+        var dealerCard1 = document.getElementById("dealerC1");
+        dealerCard1.innerHTML = players[1].hand[0];
+    })
+
+
+    } else {
     alert("Uh Oh! There can't be a poker game without any players.");
   }
 }
